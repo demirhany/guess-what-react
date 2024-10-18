b9e25c0b2edb3014fd4338182c82acb882192aa3
demirhany <demirhanyilal0@gmail.com>
2024-10-18 16:40:20 +0300

    Update: Win component has been changed

--- src/components/Win.js
+++ src/components/Win.js
@@ -1,12 +1,12 @@
-function WinModal() {
+function Win() {
+    alert("You Win!");
     const handleButtonClick = () => {
         window.location.reload();
     }
-    alert("You Win!");
     return (
         <button onClick={handleButtonClick} type="button" className="btn btn-primary mt-2">Try Again!
         </button>
     )
 }
 
-export default WinModal;
+export default Win;
