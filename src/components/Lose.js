8ba7d264b1dc25dc9277282896dded85e7082b61
demirhany <demirhanyilal0@gmail.com>
2024-10-22 14:12:08 +0300

    Feat: More beautiful lose page

--- src/components/Lose.js
+++ src/components/Lose.js
@@ -1,11 +1,21 @@
-function Lose() {
-    alert("You Lose!");
+function Lose({word}) {
     const handleButtonClick = () => {
         window.location.reload();
     }
     return (
-        <button onClick={handleButtonClick} type="button" className="btn btn-primary mt-2">Try Again!
-        </button>
+        <div>
+            <div className="row">
+                <div className="col-6">
+                    <h1 className={"fs-1 "}>Game Over!</h1>
+                </div>
+                <div className="col-6">
+                    <h1 className={"fs-1"}>Correct Word: <span className={"lose correct-word-display"}>{word}</span></h1>
+                </div>
+            </div>
+
+            <button onClick={handleButtonClick} type="button" className="btn btn-primary mt-2">Try Again!
+            </button>
+        </div>
     )
 }
 
