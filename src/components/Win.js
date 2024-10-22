fb93bb28833acf3b395ff519fab4fcee76a3b54a
demirhany <demirhanyilal0@gmail.com>
2024-10-22 14:12:53 +0300

    Feat: More beautiful win page and stylin

--- src/components/Win.js
+++ src/components/Win.js
@@ -1,12 +1,24 @@
-function Win() {
-    alert("You Win!");
+function Win({word}) {
     const handleButtonClick = () => {
         window.location.reload();
     }
     return (
-        <button onClick={handleButtonClick} type="button" className="btn btn-primary mt-2">Try Again!
-        </button>
+        <div>
+            <div className="row">
+                <div className="col-6">
+                    <h1 className={"fs-1"}>Congrats! You won!</h1>
+                </div>
+                <div className="col-6">
+                    <h1 className={"fs-1"}>Correct Word: <span className={"win correct-word-display"}>{word}</span>
+                    </h1>
+                </div>
+            </div>
+
+            <button onClick={handleButtonClick} type="button" className="btn btn-primary mt-2">Try Again!
+            </button>
+        </div>
     )
 }
+
 
 export default Win;
