43b5d1224c8dbfb6293b4f9f2a66e7bf65c44d00
demirhany <demirhanyilal0@gmail.com>
2024-11-06 11:12:52 +0300

    Update: Class names have been updated wrt. styling

--- src/components/WordBoxes.js
+++ src/components/WordBoxes.js
@@ -3,28 +3,30 @@ import {characterAssignment} from "../utils/WordBoxesLogic";
 import {LetterBox} from "../utils/WordBoxesLogic";
 
 function WordBoxes({words, randomWord}) {
-    return (<div>
-        <div className="row">
-            <h1 className={"fs-4"}>First Letter: {randomWord[0]}</h1>
+    return (
+        <div className="game-container">
+            <div className="row">
+                <h1 className={"fs-4"}>First Letter: {randomWord[0]}</h1>
+            </div>
+            {words.map((word, i) => {
+                let counts = countLetters(randomWord);
+                let characters = [];
+
+                characterInitialization(characters, word);
+                characterAssignment(word, counts, characters, randomWord);
+
+                return (<div className="row" key={"b" + i}>
+                    <div className={"d-flex justify-content-center align-items-center"} key={"a" + i}>
+                        {word.split('').map((char, j) => {
+                            return (<LetterBox char={char} isCorrect={characters[j].isCorrect} isEmpty={char === '\0'}
+                                               isInWord={characters[j].isInWord} isNotInWord={characters[j].isNotInWord}
+                                               key={"c" + i + j}/>)
+                        })}
+                    </div>
+                </div>)
+            })}
         </div>
-        {words.map((word, i) => {
-            let counts = countLetters(randomWord);
-            let characters = [];
-
-            characterInitialization(characters, word);
-            characterAssignment(word, counts, characters, randomWord);
-
-            return (<div className="row" key={"b" + i}>
-                <div className={"d-flex justify-content-center align-items-center"} key={"a" + i}>
-                    {word.split('').map((char, j) => {
-                        return (<LetterBox char={char} isCorrect={characters[j].isCorrect} isEmpty={char === '\0'}
-                                           isInWord={characters[j].isInWord} isNotInWord={characters[j].isNotInWord}
-                                           key={"c" + i + j}/>)
-                    })}
-                </div>
-            </div>)
-        })}
-    </div>);
+    );
 }
 
 export default WordBoxes;
