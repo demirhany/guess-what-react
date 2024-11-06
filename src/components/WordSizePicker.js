5158c82af01ad28e86fa943f35c9f88c630ab177
demirhany <demirhanyilal0@gmail.com>
2024-11-06 11:12:06 +0300

    Update: Cards and their logics have been updated wrt. styling

--- src/components/WordSizePicker.js
+++ src/components/WordSizePicker.js
@@ -1,22 +1,36 @@
+import {useState} from "react";
+
+const colorClasses = ['custom-card-1', 'custom-card-2', 'custom-card-3'];
+
 function WordSizePicker({onWordSizeChange, onPlayClick}) {
+    const [colorIndex, setColorIndex] = useState(0);
+
     const cards = document.getElementById("cards");
-    const handleClick = (size) => {
+    const handleClick = async (size) => {
+        setColorIndex((prevColorIndex) => (prevColorIndex + 1) % colorClasses.length);
+        await delay(3000);
         onWordSizeChange(size);
         onPlayClick(true);
         cards.innerHTML = '';
     };
 
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     return (
-        <div className="-custom-card-container" id={"cards"}>
-            <div className="custom-card" onClick={() => handleClick(5)}>
-                <img src={`${process.env.PUBLIC_URL}/images/five.png`} alt="Resim 1"/>
-            </div>
-            <div className="custom-card" onClick={() => handleClick(6)}>
-                <img src={`${process.env.PUBLIC_URL}/images/five.png`} alt="Resim 2"/>
-            </div>
-            <div className="custom-card" onClick={() => handleClick(7)}>
-                <img src={`${process.env.PUBLIC_URL}/images/five.png`} alt="Resim 3"/>
-            </div>
+        <div className="custom-card-container" id={"cards"}>
+            {colorClasses.map((colorClass, index) => (
+                <div
+                    key={index}
+                    className={`custom-card ${colorClasses[(colorIndex + index) % colorClasses.length]}`}
+                    onClick={() => handleClick(index + 5)}
+                >
+                    <span className="custom-card-title fs-2">
+                        {index + 5} {/* For displaying numbers 5, 6, and 7 */}
+                    </span>
+                </div>
+            ))}
         </div>
     )
 }
